cbc8f8e63c94f460f49bbbbecbffac64b39cf68b
Michelle <mtham101@gmail.com>
2016-06-21 16:29:27 -0700

    Added files to get started

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = {
+
+  init: function () {
+    app.playingAround();
+  },
+
+  playingAround: function () {
+    d3.select("body").append("p");
+
+    // <svg width="50" height="50">
+    //   <circle cx="25" cy="25" r="25" fill="green" />
+    // </svg>
+    d3.select("body")
+      .append("svg")
+      .attr("width", 50)
+      .attr("height", 50)
+      .append("circle")
+      .attr("cx", 25)
+      .attr("cy", 25)
+      .attr("r", 25)
+      .style("fill", "green");
+  }
+}
