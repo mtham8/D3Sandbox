9afbe6df5bd7f4a5d2d8fda65d9989cb611292d5
Michelle <mtham101@gmail.com>
2016-06-27 17:33:54 -0700

    created bar graph

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = {
     app.groupingEl();
     app.text();
     app.axes();
-    app.classes();
+    app.barGraph();
     app.dataDriven();
 
   },
@@ -282,23 +282,40 @@ const app = {
       .call(xAxis)
   },
 
-  classes: () => {
-    // let dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
-    //             14, 11, 22, 29, 11 ];
-    let dataset = [];
-    for (var i = 0; i < 15; i++) {
-      let newNum = Math.random() * 30;
-      dataset.push(newNum);
-    }
-    let bars = d3.select("body").selectAll("div")
+  barGraph: () => {
+    let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+    // let dataset = []; // create random data set
+    // for (var i = 0; i < 20; i++) {
+    //   let newNum = Math.random() * 30;
+    //   dataset.push(newNum);
+    // }
+    let w = 500;
+    let h = 100;
+    let barPadding = 1;
+    let svg = d3.select("body").append("svg")
+      .attr("height", h)
+      .attr("width", w)
+    let bars = svg.selectAll("rect")
       .data(dataset)
       .enter()
-      .append("div")
-      .attr("class", "bar")
-      .style("height", d => {
-        var barHeight = d * 5;
-        return barHeight + "px";
-      })
+      .append("rect")
+      .attr("x", (d, i) => i * (w/dataset.length) ) // create evenly spaced bars
+      .attr("y", d => h - (d * 4) ) // set the bar graph to be from bottom-up, instead of up-bottom
+      .attr("width", w/dataset.length - barPadding) // create dynamic width based on number of data values
+      .attr("height", d => d * 4 )
+      .attr("fill", d => "rgb(0, 0, " + (d * 10) + ")" ) // create height of bars based on data value
+    let text = svg.selectAll("text")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .text(d => d)
+      .attr("x", (d, i) => i * (w/dataset.length) + (w/dataset.length - barPadding) / 2) // set x to the left edge of each bar plus half the bar width
+      .attr("y", d => h - (d * 4) + 14 )
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("fill", "white")
+      .attr("text-anchor", "middle");
   },
 
   dataDriven: () => {
