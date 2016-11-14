f14d62b47a7d53a46cb298ac966ff796043e7c01
Michelle <mtham101@gmail.com>
2016-11-14 15:55:33 -0800

    D3V4: create DOM elements

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 // document.body.innerHTML = `<h1>D3 version: ${d3.version}</h1>`;
 
+// ======= CONVERT INPUT DATA TO OUTPUT VALUES ===========
 // linear scale ==> takes a continous input `domain` and maps it to a continuous output `range`, while maintaining proportions
 const linearScale = d3.scaleLinear()
   .domain([0, 100])
@@ -14,6 +15,7 @@ console.log(linearScale(105))
 console.log(linearScale.invert(300))
 */
 
+// ======= CONVERT DATES TO NUMERICAL VALUES WITH TIME SCALES ===========
 const timeScale = d3.scaleTime()
   .domain([new Date(2016, 0, 1), new Date()])
   .range([0, 100])
@@ -23,6 +25,7 @@ console.log(timeScale(new Date(2016, 4, 15)))
 console.log(timeScale(new Date()))
 */
 
+// ======= CREATE LABELS FROM NUMERIC DATA WITH QUANTIZE SCALES ===========
 // quantize scale ==> good to use if you want to map all of your input data to a specific set of output values
 const quantizeScale = d3.scaleQuantize()
   .domain([0, 100])
@@ -34,6 +37,17 @@ console.log(quantizeScale(88))
 console.log(quantizeScale.invertExtent('white')) // white is going to be anything from 33.3-66.6
 */
 
+// ======= CREATE LABELS FROM NON-NUMERIC DATA WITH ORDINAL SCALES ===========
+const ordinalScale = d3.scaleOrdinal()
+  .domain(['poor', 'good', 'great'])
+  .range(['red', 'white', 'green'])
+/*
+console.log(ordinalScale('good'))
+console.log(ordinalScale('great'))
+console.log(ordinalScale('poor'))
+*/
+
+// ======= LOAD AND INSPECT DATA ===========
 // loading data ==> d3.json
 const dataJSON = d3.json('data.json', data => {
   // get minimum element
@@ -59,6 +73,7 @@ const dataJSON = d3.json('data.json', data => {
 })
 // console.log(dataJSON)
 
+// ======= SELECT AND MODIFY DOM ELEMENTS ===========
 const div = d3.select('div')
 // to select multiple items ==> d3.selectAll
 // console.log(div.nodes())
@@ -67,15 +82,35 @@ const divLinks = div.selectAll('a')
 // alternatively ==> console.log(d3.selectAll('div a').nodes()) // [a, a, a]
 const actionLink = d3.select('.action')
 // console.log(actionLink.nodes())
+
+// add attributes or style to element
 const secondLink = d3.select('a:nth-child(2)')
-// add an attribute to an element
-secondLink.attr('href', 'http://google.com')
-console.log(secondLink.nodes())
+  .attr('href', 'http://google.com')
+  // .style('color', 'red')
+  .classed('red', true) // adds CSS classes to element
+  // .text('Inventory') // changes the text to 'Inventory'
+  .html('Inventory <b>SALE</b>')
+// console.log(secondLink.attr('href'))
 
+// ======= CREATE DOM ELEMENTS ===========
+/*
+var button = d3.select('.title')
+  // .append('button') // changes the selected element
+  .insert('button', 'a:first-child') // insert has a 'type' and `before` arg
+    .html('Inventory <b>SALE</b>')
+*/
 
+// remove element
+// d3.select('.action').remove()
+d3.select('.title')
+  .append('div')
+    .style('color', 'green')
+    .html('Inventory <b>SALE</b>')
+  .append('button')
+    .style('display', 'block')
+    .text('submit')
 
-
-
+// ======= START VISUALIZING D3 ===========
 
 
 
