336008a80433fff3b750be3243a80bfa51ce0b73
Michelle <mtham101@gmail.com>
2016-11-14 14:19:08 -0800

    D3V4: set scale using data extent

--- client/app.js
+++ client/app.js
@@ -35,8 +35,27 @@ console.log(quantizeScale.invertExtent('white')) // white is going to be anythin
 */
 
 // loading data ==> d3.json
-const dataJSON = d3.json('data.json', function (d){
-  console.log(d);
+const dataJSON = d3.json('data.json', data => {
+  // get minimum element
+  let min = d3.min(data, d => d.age)
+  // console.log('this is min ==> ', min)
+
+  // get maximum element
+  let max = d3.max(data, d => d.age)
+  // console.log('this is max ==> ', max)
+
+  // get min and max
+  let extent = d3.extent(data, d => d.age)
+  // console.log('this is both ==> ', extent)
+
+  let scale = d3.scaleLinear()
+    .domain(extent)
+    .range([0, 600])
+    // console.log('this is scaled value ==> ', scale(24))
+
+  // for unique values, use d3.set()
+  let ages = d3.set(data, d => d.age)
+  // console.log('this is set of unique values ==> ', ages.values())
 })
 // console.log(dataJSON)
 
