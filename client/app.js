faa94423933fd8e2e507968e4458bd08cf290778
Michelle <mtham101@gmail.com>
2016-11-19 21:34:07 -0800

    D3V4: start visualizing D3 pt 2

--- client/app.js
+++ client/app.js
@@ -123,15 +123,23 @@ const update = d3.select('.chart')
   .selectAll('div')
   .data(scores, function(d){
     return d ? d.name : this.innerText
-  }) // to make sure data already on the DOM doesn't get repeated
+  }) // to make sure data already on the DOM doesn't get repeated ==> this is set the the DOM element
   .style('color', 'blue')
 
-update.enter()
+const enter = update.enter() // this adds new elements
   .append('div') // this tells d3 to append a div to each of the data items that aren't appended to an element
   .text(d => d.name)
     .style('color', 'green')
 
-update.exit().remove()
+update.exit().remove() // this removes element that don't have data appended
+
+update.merge(enter)
+  .style('width', d => `${d.score}px`)
+  .style('height', '50px')
+  .style('background', 'lightgreen')
+  .style('border', '1px solid black')
+
+
 
 
 
