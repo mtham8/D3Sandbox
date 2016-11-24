2c2351785632f7a0c6bfade7051fe232650bac88
Michelle Tham <mtham101@gmail.com>
2016-11-23 20:26:22 -0800

    D3V4: Animate transitions

---
+++ src/components/animate-transition.js
@@ -0,0 +1,17 @@
+// ======= ANIMATE TRANSITIONS ===========
+
+/*
+d3.select('#block')
+  .transition()
+    .duration(600)
+    .delay(750)
+    .ease(d3.easeCubicOut)
+    .style('width', '400px')
+  .transition()
+    .duration(500)
+    .ease(d3.easeCubicOut)
+    .style('height', '600px')
+  .transition()
+    .duration(2000)
+    .style('background-color', 'teal')
+*/
